6a34e7b4636c3dffe8b87bb132e69ffcbde6662a
Horiyasiddique <horiyasiddique52@gmail.com>
2025-05-06 14:22:08 +0500

    done

---
+++ 04_JavaScript/1-hello world/script.js
@@ -0,0 +1,9 @@
+// Chapter 1 in JS (console.log) & (console.table)
+console.log(`Hello World`);
+
+console.log("Hooriya Siddiqui");
+
+console.log("Wariha Ayyub Khan");
+
+console.table(['horiya', 'wariha', 'Aina', 'Abdul Hannan', 'Sajda Khosk']);
+
