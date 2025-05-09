13755bbc850c550fe6ebe53c6e179d41791b61df
Horiyasiddique <horiyasiddique52@gmail.com>
2025-05-09 22:48:40 +0500

    variables in JS

---
+++ 04_JavaScript/2-variables/script.js
@@ -0,0 +1,35 @@
+/**
+ * MEMORIES IN JS :
+ * 1.STACK  2.HEAP MEMORY
+ * stack memory : large in size 
+ */
+
+/**
+ *  Variables : 1. let 2.var 3. const
+ * 1.let : 
+ * ⭐can be changes (reassigned)
+ * ⭐can not be redeclare
+ * ⭐block scope
+ * 
+ * 2. const:
+ * ⭐can not be changed (reassigned) it is constant
+ * ⭐can note be redeclare
+ * ⭐block scope
+ * 
+ * 3. var
+ * ⭐can be change (reassigned)
+ * ⭐can be redeclare
+ * ⭐function scope
+ * ⭐not a good practice (older version)
+ */
+
+const name = 'Hooriya siddiqui'
+// name = 'wariha khan' ❌❌❌❌❌ Error can't reassign to constant 
+
+// initialization
+let age = 14
+//reassign
+age = 15
+
+var friends = 1
+var friends = 3
