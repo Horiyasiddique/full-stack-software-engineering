30ceb5ebe0f7800c49ba5e6a596537df49f0411a
Horiyasiddique <horiyasiddique52@gmail.com>
2025-05-13 07:01:52 +0500

    data types in JS

---
+++ 04_JavaScript/3-data types/script.js
@@ -0,0 +1,31 @@
+/**
+ * DATA TYPES
+ * data types are divided into 2 categories
+ * 1. Primitive (string, number, boolean)
+ * 2. Non Primitive (Bigint, undefined, null....)
+ */
+
+// String : anything which is written in "", '', `` is called string
+let first_name = "Hooriya";
+let last_name = "Siddiqui";
+let passion = `coding`;
+
+// Number : any number
+let marks = 90;
+//but what if I wrote the number in "" or '' or `` ?????????
+let age = '14';
+console.log(typeof age);
+
+// Boolean : true or false 
+let isMarried = false;
+let isHuman = true;
+
+// undefined : means variable don't have value 
+let mood;
+console.log(mood)
+console.log(typeof mood)
+
+/**
+ * difference b/w pointer & value : 
+ * a variable directly stores a value, while a pointer stores the location where a value is stored.
+ */
